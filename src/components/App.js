80913245697d5504aaa5a52307a47664f646a1ad
Beatrice Kibor <kibor9346@gmail.com>
2025-07-14 20:31:11 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+const user = {
+  name: "Liza",
+  city: "New York",
+  bio: "I made this!",
+  color: "firebrick",
+  links: {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  },
+};
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav></nav> 
+        <Home username={user.name} city={user.city} color={user.color} />
+        <About bio={user.bio} color={user.color} links={user.links} />
+      </div>
+    );
+  }
 }
 
 export default App;
